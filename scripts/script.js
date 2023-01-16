e621eb9753208a1b91adb196165b6bad7d2f2d9a
Arthur Naumchyk <arthur.naumchik@gmail.com>
2023-01-16 16:02:32 +0200

    90-precent-done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-console.log("Слава Україні")
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
